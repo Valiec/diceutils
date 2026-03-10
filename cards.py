3aa3cbf303eaa7223c8ced486527435382801c17
Valiec <valiec2019@gmail.com>
2026-03-10 09:40:21 -0400

    Card stuff WIP

---
+++ cards.py
@@ -0,0 +1,150 @@
+import random
+
+class CardsData:
+    def __init__(self, games):
+        self.games = games
+
+    def add_game(self, game):
+        self.games.append(game)
+
+    def serialize(self):
+        return [game.serialize() for game in self.games]
+
+    @staticmethod
+    def deserialize(cards_data):
+        loaded_games = {}
+        for game in cards_data:
+            loaded_game = Game.deserialize(game)
+            loaded_games[loaded_game.name] = loaded_game
+        return CardsData(loaded_games)
+
+class Game:
+    def __init__(self, name, decks, hands):
+        self.name = name
+        self.decks = decks
+        self.hands = hands
+
+    def add_deck(self, deck):
+        self.decks.append(deck)
+
+    def serialize(self):
+        return {"name": self.name, "decks": [deck.serialize() for deck in self.decks],
+                "hands": [hand.serialize() for hand in self.hands]}
+
+    @staticmethod
+    def deserialize(game_data):
+        loaded_decks = {}
+        for deck in game_data["decks"]:
+            loaded_deck = Deck.deserialize(deck)
+            loaded_decks[loaded_deck.name] = loaded_deck
+        loaded_hands = {}
+        for hand in game_data["hands"]:
+            loaded_hand = Hand.deserialize(hand)
+            loaded_hands[loaded_hand.member] = loaded_hand
+        return Game(game_data["name"], loaded_decks, loaded_hands)
+
+class Hand:
+    def __init__(self, cards, member):
+        self.cards = cards
+        self.member = member
+
+    def add_card(self, card):
+        self.cards.append(card)
+
+    def remove_card(self, index):
+        return self.cards.remove(self.cards[index])
+
+    def serialize(self):
+        return {"member": self.member, "cards": [card.serialize() for card in self.cards]}
+
+    @staticmethod
+    def deserialize(hand_data):
+        loaded_cards = []
+        for card in hand_data:
+            loaded_cards.append(Card.deserialize(card))
+        return Hand(loaded_cards, hand_data["member"])
+
+class Deck:
+    def __init__(self, name, cards, drawn, discard):
+        self.name = name
+        self.cards = cards
+        self.drawn = drawn
+        self.discard = discard
+
+    def shuffle(self):
+        random.shuffle(self.cards)
+
+    def draw(self):
+        self.drawn.append(self.cards.pop())
+
+    def discard(self, card):
+        self.discard.append(card)
+
+    def serialize(self):
+        return {"name": self.name, "cards": [card.serialize() for card in self.cards],
+                "drawn": [card.serialize() for card in self.drawn],
+                "discard": [card.serialize() for card in self.drawn]}
+
+    @staticmethod
+    def init_cards_default(jokers=True):
+        cards = []
+        for suit in ["Hearts", "Diamonds", "Spades", "Clubs"]:
+            for num in ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"]:
+                cards.append(Card(suit, num))
+        if jokers:
+            cards.append(Card("red", "Joker"))
+            cards.append(Card("black", "Joker"))
+
+        return Deck(cards, [], [], [])
+
+    @staticmethod
+    def deserialize(deck_data):
+        loaded_cards = []
+        for card in deck_data["cards"]:
+            loaded_cards.append(Card.deserialize(card))
+        loaded_drawn = []
+        for card in deck_data["drawn"]:
+            loaded_drawn.append(Card.deserialize(card))
+        loaded_discard = []
+        for card in deck_data["discard"]:
+            loaded_discard.append(Card.deserialize(card))
+        return Deck(deck_data["name"], loaded_cards, loaded_drawn, loaded_discard)
+
+class Card:
+    def __init__(self, suit, num):
+        self.suit = suit
+        self.num = num
+        self.color = ""
+        self.joker = False
+
+        if self.suit == "Hearts" or self.suit == "Diamonds" or self.suit == "red":
+            self.color = "Red"
+        if self.suit == "Spades" or self.suit == "Clubs" or self.suit == "black":
+            self.color = "Black"
+        if self.num == -1:
+            self.joker = True
+
+    def full_name(self):
+        if not self.joker:
+            return f"{self.num} of {self.suit}"
+        else:
+            return f"{self.color} Joker"
+
+    def __str__(self):
+        if not self.joker:
+            suits = {"Hearts": ":hearts:", "Diamonds":":diamonds:", "Spades":":spades:", "Clubs":":clubs:"}
+            short_names = {"Ace":"A", "2":"2", "3":"3", "4":"4", "5":"5", "6":"6", "7":"7", "8":"8",
+                           "9":"9", "10":"10", "Jack":"J", "Queen":"Q", "King":"K"}
+            return f"{short_names[self.num]}{suits[self.suit]}"
+        else:
+            return f"J{':red_circle:' if self.color == 'Red' else ':black_circle:'}"
+
+
+    def serialize(self):
+        return {"suit": self.suit, "num": self.num}
+
+    @staticmethod
+    def deserialize(card_data):
+        return Card(card_data["suit"], card_data["num"])
+
+
---
+++ dice.py
@@ -0,0 +1,425 @@
+import multiprocessing
+import random
+
+
+class DiceError(Exception):
+    """dice error"""
+
+    def __init__(self, msg):
+        self.message = msg
+
+    def __str__(self):
+        return self.message
+
+
+def float_or_error(num):
+    try:
+        return float(num)
+    except ValueError:
+        raise DiceError("Invalid expression syntax")
+
+
+def roll_dice(q, dsize, ndice):
+    result = []
+    for i in range(ndice):
+        q.put(random.randint(1, dsize))
+    return result
+
+
+def valid_dice_roll(test_roll):
+    if len(test_roll) != 2:
+        return False
+    if test_roll[0].isnumeric() and test_roll[1].isnumeric() and int(test_roll[1]) > 0:
+        return True
+    return False
+
+
+def too_many_dice(test_roll):
+    if len(test_roll) > 0 and test_roll[0].isnumeric():
+        if int(test_roll[0]) > 2000:
+            return True
+        return False
+    return False
+
+
+def roll_all_dice(ndice, dsize):
+    tobjs = []
+    results = []
+    q = multiprocessing.Queue()
+    for i in range(10):
+        nd = ndice // 10
+        if i == 9:
+            nd += ndice % 10
+        t = multiprocessing.Process(target=roll_dice, args=(q, dsize, nd))
+        tobjs.append(t)
+        t.start()
+    for t in tobjs:
+        t.join()
+    for i in range(ndice):
+        results.append(q.get())
+    return results
+
+
+def is_operator(char):
+    operators = ["+", "-", "*", "/", "^"]
+    return char in operators
+
+
+def combine_ops(cur, prev):
+    print("combining: "+str(cur)+", "+str(prev)+"?")
+    if cur == "+" and prev == "-" or cur == "-" and prev == "+":
+        return "-"
+    elif cur == "-" and prev == "-":
+        return "+"
+    elif cur == "+" and prev == "+":
+        return "+"
+    else:
+        return None
+
+
+def collapse_repeated_ops(tokens):
+    final = []
+    i = len(tokens)-1
+    while i >= 0:
+        cur = tokens[i]
+        skip_prepend = False
+        print("testing, cur: " + str(cur)+", final: " + str(final)+" cur is op: "+str(is_operator(cur))
+              +" final[-1] is op: "+str(is_operator(final[0] if len(final) > 0 else "None")))
+        if is_operator(cur) and len(final) > 0 and is_operator(final[0]):
+            print("trying to combine, final is: "+str(final))
+            new_op = combine_ops(cur, final[0])
+            if new_op is not None:
+                final[0] = new_op
+                print("combined, final is now: " + str(final))
+                skip_prepend = True
+
+        if not skip_prepend:
+            final = [cur] + final  # prepend cur to final
+        i -= 1
+    return final
+
+
+def tokenize(text):
+    tokens = []
+    paren_token = ""
+    paren_level = 0
+    cur_token_str = ""
+    for char in text:
+        if char.isspace():
+            continue
+        elif char == "(":
+            if paren_level == 0:
+                if cur_token_str != "":
+                    tokens.append(cur_token_str[:])
+                    cur_token_str = ""
+            else:
+                paren_token = paren_token + "("
+            paren_level += 1
+        elif char == ")":
+            paren_level -= 1
+            if paren_level == 0:
+                tokens.append(tokenize(paren_token))
+                paren_token = ""
+            else:
+                paren_token = paren_token + ")"
+        elif paren_level > 0:
+            paren_token = paren_token + char
+        elif not is_operator(char):
+            if cur_token_str != "" and is_operator(cur_token_str[-1]):
+                tokens.append(cur_token_str[:])
+                cur_token_str = ""
+            cur_token_str = cur_token_str + char
+        else:
+            if cur_token_str != "":
+                tokens.append(cur_token_str[:])
+            cur_token_str = char
+
+    if cur_token_str != "":
+        tokens.append(cur_token_str[:])
+
+    collapsed = collapse_repeated_ops(tokens)
+    return collapsed
+
+
+def eval_exp(tokens):
+    tokens_pass1 = (tokens[:])
+    tokens_pass1.reverse()
+    tokens_pass2 = []
+    first_pass = True
+    while "^" in tokens_pass1 or first_pass:
+        first_pass = False
+        last_token = ""
+        last_op = ""
+        tokens_pass2 = []
+        for token in tokens_pass1:
+            if token == "^" and last_op != "":
+                raise DiceError("Invalid expression syntax")
+            elif token == "^":
+                last_op = "^"
+            elif last_op != "":
+                # tokens_pass2.append(str(float(last_token)**float(token)))
+                if last_token == "" or token == "":
+                    raise DiceError("Invalid expression syntax")
+                last_token = str(float_or_error(token) ** float_or_error(last_token))
+                last_op = ""
+            else:
+                if last_token != "":
+                    tokens_pass2.append(last_token)
+                last_token = token
+        if last_token != "":
+            tokens_pass2.append(last_token)
+        tokens_pass1 = tokens_pass2[:]
+        if last_op != "":
+            raise DiceError("Invalid expression syntax")
+    tokens_pass2.reverse()
+    return tokens_pass2
+
+
+def eval_mul_div(tokens):
+    tokens_pass2 = tokens[:]
+    tokens_pass3 = []
+    first_pass = True
+    while "*" in tokens_pass2 or "/" in tokens_pass2 or first_pass:
+        first_pass = False
+        last_token = ""
+        last_op = ""
+        tokens_pass3 = []
+        for token in tokens_pass2:
+            if (token == "*" or token == "/") and last_op != "":
+                raise DiceError("Invalid expression syntax")
+            elif token == "*":
+                last_op = "*"
+            elif token == "/":
+                last_op = "/"
+            elif last_op == "*":
+                # tokens_pass3.append(str(float(last_token)*float(token)))
+                if last_token == "" or token == "":
+                    raise DiceError("Invalid expression syntax")
+                last_token = str(float_or_error(last_token) * float_or_error(token))
+                last_op = ""
+            elif last_op == "/":
+                # tokens_pass3.append(str(float(last_token)/float(token)))
+                if last_token == "" or token == "":
+                    raise DiceError("Invalid expression syntax")
+                last_token = str(float_or_error(last_token) / float_or_error(token))
+                last_op = ""
+            else:
+                if last_token != "":
+                    tokens_pass3.append(last_token)
+                last_token = token
+        if last_token != "":
+            tokens_pass3.append(last_token)
+        tokens_pass2 = tokens_pass3[:]
+        if last_op != "":
+            raise DiceError("Invalid expression syntax")
+    return tokens_pass3
+
+
+def eval_add_sub(tokens):
+    tokens_pass3 = tokens[:]
+    tokens_pass4 = []
+    first_pass = True
+    while "+" in tokens_pass3 or "-" in tokens_pass3 or first_pass:
+        first_pass = False
+        print("tokens_pass3 is " + str(tokens_pass3))
+        last_token = ""
+        last_op = ""
+        tokens_pass4 = []
+        for token in tokens_pass3:
+            if token == "+":
+                last_op = "+"
+            elif token == "-":
+                last_op = "-"
+            elif last_op == "+":
+                # tokens_pass4.append(str(float(last_token)+float(token)))
+                if last_token == "":
+                    last_token = "0"
+                if token == "":
+                    raise DiceError("Invalid expression syntax")
+                print("A")
+                last_token = str(float_or_error(last_token) + float_or_error(token))
+                last_op = ""
+            elif last_op == "-":
+                if last_token == "":
+                    last_token = "0"
+                if token == "":
+                    raise DiceError("Invalid expression syntax")
+                # tokens_pass4.append(str(float(last_token)-float(token)))
+                print("S")
+                last_token = str(float_or_error(last_token) - float_or_error(token))
+                last_op = ""
+            else:
+                if last_token != "":
+                    tokens_pass4.append(last_token)
+                last_token = token
+        if last_token != "":
+            tokens_pass4.append(last_token)
+        tokens_pass3 = tokens_pass4[:]
+        if last_op != "":
+            raise DiceError("Invalid expression syntax")
+    return tokens_pass4
+
+
+def process_dice(token):
+    if token[0].isnumeric():
+        token = "sum" + token
+
+    func = token[:3]
+    token = token[3:]
+    token_str = token
+    token = token.split("d")
+    print(func)
+    print(token)
+
+    valid = valid_dice_roll(token)
+    too_many = too_many_dice(token)
+
+    if valid and not too_many:
+        rollvals = roll_all_dice(int(token[0]), int(token[1]))
+    elif not valid:
+        raise DiceError("Invalid dice roll: " + token_str)
+    elif too_many:
+        raise DiceError("Too many dice: " + token_str)
+    else:
+        raise DiceError("Error: " + token_str)
+
+    print(rollvals)
+
+    if func == "adv":
+        result = max(rollvals)
+    elif func == "dis":
+        result = min(rollvals)
+    elif func == "avg":
+        result = sum(rollvals) / float(len(rollvals))
+    else:
+        result = sum(rollvals)
+
+    return str(result)
+
+
+def evaluate(tokens_pass0):
+    print("Pass 0: " + str(tokens_pass0))
+
+    tokens_pass1 = []
+    for token in tokens_pass0:
+        if isinstance(token, list):
+            tokens_pass1.append(str(evaluate(token)))
+        else:
+            tokens_pass1.append(token)
+
+    print("Pass 1: " + str(tokens_pass1))
+
+    tokens_pass1b = []
+    for token in tokens_pass1:
+        if "d" in token:
+            tokens_pass1b.append(process_dice(token))
+        else:
+            tokens_pass1b.append(token)
+
+    print("Pass 1b: " + str(tokens_pass1b))
+
+    tokens_pass2 = eval_exp(tokens_pass1b)
+
+    print("Pass 2: " + str(tokens_pass2))
+
+    tokens_pass3 = eval_mul_div(tokens_pass2)
+
+    print("Pass 3: " + str(tokens_pass3))
+
+    tokens_pass4 = eval_add_sub(tokens_pass3)
+
+    print("Pass 4: " + str(tokens_pass4))
+
+    if len(tokens_pass4) != 1:
+        raise DiceError("Invalid expression")
+
+    return tokens_pass4[0]
+
+def roll_init_helper(turns, starts, incrs, names, debug):
+    totals = starts[:]
+    turn_order = []
+    turn_totals_pre = []
+    turn_totals_post = []
+    round_starts = [0]  # turn numbers (zero-based) just before which a new round starts
+    taken_turns = [False] * len(names)
+    for i in range(turns):
+        current = get_max_initiative(totals, incrs)
+        turn_totals_pre.append("[" + (",".join([str(val) for val in totals])) + "]")
+        turn_order.append(current)
+        totals = [totals[j] + incrs[j] for j in range(len(totals))]
+        totals[current] = 0
+        turn_totals_post.append("[" + (",".join([str(val) for val in totals])) + "]")
+        taken_turns[current] = True  # has taken a turn
+        if False not in taken_turns:  # everyone has taken a turn, round over
+            round_starts.append(len(turn_order))  # record round start after this turn
+            taken_turns = [False] * len(names)  # reset taken_turns
+
+    turn_log = []
+    for i in range(len(turn_order)):
+        new_turn = ""
+        if i in round_starts:
+            new_turn += "=== NEW ROUND ===\n"
+        new_turn += "Turn " + str(i + 1) + ": " + names[turn_order[i]]
+        if debug:
+            new_turn += " (debug: " + turn_totals_pre[i] + " -> " + turn_totals_post[i] + ")"
+        turn_log.append(new_turn)
+
+    return "\n".join(turn_log)
+
+
+async def roll_init_cmd_helper(interaction, turns, starts, increments, names, debug):
+    try:
+        incrs_l = [int(val) for val in increments.split(",")]
+        if "," in starts:
+            starts_l = [int(val) for val in starts.split(",")]
+        elif starts == "-":
+            starts_l = incrs_l[:]
+        else:
+            raise ValueError("oops")
+
+        names_l = names.split(",")
+        turn_order = roll_init_helper(turns, starts_l, incrs_l, names_l, debug)
+        if len(turn_order) < 2000:
+            await interaction.response.send_message(turn_order)
+        else:
+            await interaction.response.send_message("Output would exceed 2000 characters", ephemeral=True)
+    except ValueError:
+        await interaction.response.send_message("Argument syntax error", ephemeral=True)
+
+
+def get_max_initiative(totals, additives):
+    max_val = -1  # initiative totals are nonnegative
+    max_inds = []  # indices where the maximum value occurs
+    for i in range(len(totals)):
+        # new maximum, reset occurrences
+        if totals[i] > max_val:
+            max_val = totals[i]
+            max_inds = [i]
+        # repeat of current max, add to list
+        elif totals[i] == max_val:
+            max_inds.append(i)
+
+    # if there is only one maximum, return it
+    if len(max_inds) == 1:
+        return max_inds[0]
+
+    # if there is a tie, try to use the additive to break the tie
+
+    max_add = 0  # additives are positive
+    max_add_inds = []  # indices where max additive occurs
+
+    for i in range(len(max_inds)):
+        # new maximum, reset occurrences
+        if additives[max_inds[i]] > max_add:
+            max_add = additives[max_inds[i]]
+            max_add_inds = [max_inds[i]]
+        # repeat of current max, add to list
+        elif additives[max_inds[i]] == max_add:
+            max_add_inds.append(max_inds[i])
+
+    # if there is now only one maximum, return it
+    if len(max_add_inds) == 1:
+        return max_add_inds[0]
+
+    # else, randomly pick among the remaining ties (tied with both total and additive)
+    return random.choice(max_add_inds)
