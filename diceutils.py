ecd8528fe8cc97fbbc60510fa182affd277cf254
Valiec <valiec2019@gmail.com>
2026-03-10 16:27:35 -0400

    1-based indexing

--- diceutils.py
+++ diceutils.py
@@ -264,7 +264,7 @@ async def discard(interaction, deck: str, index: int):
     """Discards a card."""
     if interaction.user.id not in card_data.games[deck].hands:
         await interaction.response.send_message("You do not have a hand for this deck.", ephemeral=True)
-    elif index < 0 or index >= len(card_data.games[deck].hands[interaction.user.id].cards):
+    elif index < 1 or index > len(card_data.games[deck].hands[interaction.user.id].cards):
         await interaction.response.send_message("Card index out of range.", ephemeral=True)
     else:
         card = card_data.games[deck].hands[interaction.user.id].discard(index-1, deck)
